b189da773c959d8e1c63fed01726bf187aba2e6b
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-04-27 20:24:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,18 @@
 # Write your code here!
+
+def game_hash
+  nested_hash = {
+    :home => {
+      :team_name => "",
+      :colors => [],
+      :players => [
+        {
+        :player_name => ""
+        },
+        {},{}]
+    },
+    :away => {}
+    
+  }
+  
+end
